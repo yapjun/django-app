620818b22d1ee4d6d912bf42e9659a3b953780a2
yapjun <daphneyapjy@hotmail.com>
2023-02-11 21:19:47 +0000

    tutorial 03

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+from django.http import Http404
+from .models import Question
+from django.shortcuts import get_object_or_404, render
 
+# this is one way to do it
+"""def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))"""
+
+## and this is another way of doing the same shit but using render()
 def index(request):
-        return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+# raises http404 exception if a question with the requested ID does not exist
+"""def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+"""
+
+# another way of doing 404 but shorter, wow so short
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
